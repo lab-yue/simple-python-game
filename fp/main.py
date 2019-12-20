1edb4a02290883711e0e685f17652e88bb06a445
rainy-me <github@rainy.me>
2019-12-21 02:47:53 +0900

    use a little decorator

--- fp/main.py
+++ fp/main.py
@@ -34,12 +34,18 @@ def is_enemy(a, b):
     return a['group'] != b['group']
 
 
+def info(f):
+    def wrapper():
+        print("start")
+        f()
+        print("end")
+    return wrapper
+
 def game(characters):
     __turn_count = 0
-    __character_len = len(characters)
 
     def current_character():
-        return characters[__turn_count % __character_len]
+        return characters[__turn_count % len(characters)]
 
     def enemy_character():
         current = current_character()
@@ -48,9 +54,12 @@ def game(characters):
                 return maybe_enemy
         return None
 
-    def status():
-        for character in characters:
-            print(character)
+    def status(f):
+        def wrapper():
+            f()
+            for character in characters:
+                print(character)
+        return wrapper
 
     def attack(a, b):
         print(
@@ -63,21 +72,19 @@ def game(characters):
             tmp_hp = 0
             print(f"{b['name']} is dead")
         b.update({'current_hp': tmp_hp})
-
-    def run():
         nonlocal __turn_count
-        print('start')
-        while True:
-            current = current_character()
+        __turn_count += 1
+
+    @status
+    @info
+    def run():
+        while (current := current_character()):
             if (enemy := enemy_character()):
                 attack(current, enemy)
-                __turn_count += 1
             else:
                 break
-        print('end')
 
-    return run, status
-
+    return run
 
 if __name__ == '__main__':
     create_character = use_character()
@@ -85,8 +92,4 @@ if __name__ == '__main__':
     enemy1 = create_character(name='enemy1', hp=5, group=ENEMY)
     enemy2 = create_character(name='enemy2', hp=3, group=ENEMY)
 
-    run, status = game([hero, enemy1, enemy2])
-
-    status()
-    run()
-    status()
+    game([hero, enemy1, enemy2])()
